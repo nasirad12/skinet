29af35c1c71143092259d099db6cbd768c438358
nasirad <nasirdesai313@gmail.com>
2020-08-16 10:06:09 +0530

    End of Section 8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    pictureUrl: string;
+    productType: string;
+    productBrand: string;
+  }
+  
